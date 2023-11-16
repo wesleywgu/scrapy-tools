535f42d9d90c274f89d6dca48a514aaf368cf25f
tianchu <tianchu@pingduoduo.com>
2023-11-16 15:22:23 +0800

    Google news scrap complete

--- google/google/spiders/search_news.py
+++ google/google/spiders/search_news.py
@@ -8,16 +8,14 @@ except:
     from scrapy.spiders import BaseSpider as Spider
 
 import dateparser, copy
-from bs4 import BeautifulSoup as Soup, ResultSet
-from dateutil.parser import parse
-import re
+from bs4 import BeautifulSoup as Soup
 import datetime
 from dateutil.relativedelta import relativedelta
 from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
 
 class googleSpider(Spider):
-    name = "news_search"
+    name = "search_news"
     allowed_domains = ["google.com"]
     start_urls = [
         "https://www.google.com/search?q=%E6%8B%BC%E5%A4%9A%E5%A4%9A&sca_esv=581520105&tbas=0&tbs=qdr:w,sbd:1&tbm=nws&sxsrf=AM9HkKkXb3sBmvO6GPX6Bk-OFf-AauWLOA:1699710999318&ei=F4hPZeqGE5vf2roPk-C5sA4&start=0&sa=N&ved=2ahUKEwiq7tbyjLyCAxWbr1YBHRNwDuY4ChDx0wN6BAgCEAI&biw=1680&bih=825&dpr=2&hl=en",
